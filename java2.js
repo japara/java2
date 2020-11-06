d105fe3c1d0a5aeed3d780867f2455021242927d
Japara <datajaparidze@Yahoo.com>
2020-11-06 15:30:45 +0400

    Create java2.js

---
+++ java2.js
@@ -0,0 +1,20 @@
+// let array = ['html','bootstrap' ,'array','bot']
+// let result = array.filter(x=> x.length>4 && x.includes('ra'))
+// console.log(result)
+
+// let number = [25, 15, 44, 34, 33, 55, 20];
+// let numbers = number.filter(x=>x%2==0);
+// console.log(numbers);
+
+// let array = ['html','bootstrap' ,'array','bot'];
+
+// array.forEach(function(x){
+//     console.log(x.length>4);
+// }
+// )
+
+// array.forEach(x=> {
+//     if(x.lenght<4){
+//         console.log(x);}
+//     })
+
